09d09d797811b70f20b424fb95315cdf1ce91a61
TakNg <tak_ng@yahoo.com>
2017-08-01 22:06:51 +0000

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,77 @@
 import React, {Component} from 'react';
+import ChatBar     from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser : { name: 'Anonymous' },
+      messages    : [],
+      userCount: 0
+    }
+  }
+
+  componentDidMount() {
+    this.websocket = new WebSocket("ws://localhost:3001");
+    this.websocket.onopen = (event) => {
+      console.log('Connected to server');
+    };
+    this.websocket.onmessage = (event) => {
+      const userData = JSON.parse(event.data);
+      switch (userData.type) {
+        case "incomingCount":
+          this.setState({userCount: userData.count});
+          break;
+        case "incomingMessage":
+        case "incomingNotification":
+          const messages = this.state.messages.concat(userData);
+          this.setState({messages: messages});
+          break;
+        default:
+          throw new Error("Unknown message type: " + userData.type);
+      }
+    };
+  }
+
+  sendMessage = (messageEvent) => {
+    const {name, message} = messageEvent;
+    const newMessage = {type: "incomingMessage", username: name, content: message};
+
+    this.websocket.send(JSON.stringify(newMessage));
+    this.setName(name);
+  }
+
+  setName = (name) => {
+    if (name === this.state.currentUser.name) {
+      return;
+    }
+    const content = this.state.currentUser.name + " name changed to " + name;
+
+    const newUser = Object.assign({}, this.state.currentUser, { name });
+    this.state.currentUser.name = name;
+    this.setState({currentUser: newUser});
+
+    const newMessage = {type: "postNotification", content: content};
+    this.websocket.send(JSON.stringify(newMessage));
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+          <h3 className="navbar-user-count">{ this.state.userCount } users online</h3>
+        </nav>
+        <MessageList messages={this.state.messages}/>
+        <ChatBar
+          currentUser={this.state.currentUser}
+          onMessageSend={this.sendMessage}
+         />
+      </div>
     );
   }
 }
+
 export default App;
+
