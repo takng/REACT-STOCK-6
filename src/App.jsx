a466591b7761d964bac8a2065ab55fc46b747864
Ayesha <ayesha_04@yahoo.com>
2017-08-15 16:13:56 +0000

    added few more columns to table

--- src/App.jsx
+++ src/App.jsx
@@ -286,11 +286,18 @@ if (Object.keys(stocks).length > 0) {
         <table>
           <thead>
             <tr>
-              <th>Date</th>
+              <th>Name</th>
               <th>Symbol</th>
               <th>Price</th>
+              <th>Open</th>
               <th>High</th>
               <th>Low</th>
+              <th>52 Week High</th>
+              <th>52 Week Low</th>
+              <th>Volume</th>
+              <th>Market Cap</th>
+              <th>Dividend Yield</th>
+
             </tr>
           </thead>
           <tbody>
@@ -298,8 +305,14 @@ if (Object.keys(stocks).length > 0) {
               <td>{stocks.longName}</td>
               <td>{stocks.symbol}</td>
               <td>{stocks.regularMarketPrice}</td>
+              <td>{stocks.regularMarketOpen}</td>
               <td>{stocks.regularMarketDayHigh}</td>
               <td>{stocks.regularMarketDayLow}</td>
+              <td>{stocks.fiftyTwoWeekHigh}</td>
+              <td>{stocks.fiftyTwoWeekLow}</td>
+              <td>{stocks.regularMarketVolume}</td>
+              <td>{stocks.marketCap}</td>
+              <td>{stocks.trailingAnnualDividendYield}</td>
             </tr>
           </tbody>
         </table>
