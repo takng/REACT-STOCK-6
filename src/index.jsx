a3758cb49ef57d260fd920dac0711ad59ead499e
TakNg <tak_ng@yahoo.com>
2017-08-16 18:06:08 +0000

    COLOR

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Fetch from 'react-fetch';
-import Request from 'superagent';
+//import Request from 'superagent';
 
 
 
